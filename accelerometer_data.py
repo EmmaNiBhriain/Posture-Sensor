522ebc52c267787f6c02370f9a81b38a2fe4fb46
Emma <007eob@gmail.com>
2017-11-18 16:36:34 +0000

    Initial Commit
    Libraries added and a simple program to read the values of the
    accelerometer in the pysense board.

---
+++ accelerometer_data.py
@@ -0,0 +1,12 @@
+from pysense import Pysense
+from LIS2HH12 import LIS2HH12
+from time import sleep
+
+py = Pysense()
+li = LIS2HH12(py)
+
+while True:
+    print(li.acceleration())
+    print(li.roll())
+    print(li.pitch())
+    sleep(1)
