e394fab50ab48862c36d3196ac2120ab765c3f6a
Emma <007eob@gmail.com>
2017-11-22 10:54:43 +0000

    Send accelerometer roll values to dashboard.
    The sending_wifi_data.py file now sends the roll values of the Pysense
    board to the dashboard over WiFi every three seconds.

--- accelerometer_data.py
+++ accelerometer_data.py
@@ -2,8 +2,8 @@ import pysense
 from LIS2HH12 import LIS2HH12
 from time import sleep
 
-py = Pysense()
-li = LIS2HH12(py)
+#py = Pysense()
+li = LIS2HH12()
 
 while True:
     print(li.acceleration())
--- sending_wifi_data.py
+++ sending_wifi_data.py
@@ -1,7 +1,11 @@
 from mqtt import MQTTClient
 from network import WLAN
+import pysense
+from LIS2HH12 import LIS2HH12 #import accelerometer library
 import machine
 import time
+
+li = LIS2HH12() #initialise accelerometer
 
 def sub_cb(topic, msg):
     print(msg)
@@ -9,20 +13,21 @@ def sub_cb(topic, msg):
 wlan = WLAN(mode=WLAN.STA)
 wlan.connect("IoT2", auth=(WLAN.WPA2, "ilikecake"), timeout=5000)
 
+print("Connecting to wifi")
 while not wlan.isconnected():
     machine.idle()
 print("connected to wifi\n")
 
-client = MQTTClient("COM6", "io.adafruit.com",user="EmmaNiBhriain", password="e99caf87d89749f28926c88d7170137c", port=1883)
+client = MQTTClient("Pycom", "io.adafruit.com",user="EmmaNiBhriain", password="e99caf87d89749f28926c88d7170137c", port=1883)
 client.set_callback(sub_cb)
 client.connect()
 client.subscribe(topic="EmmaNiBhriain/feeds/x-accelerometer")
 
 while True:
-    print("sending value 90")
-    client.publish(topic="EmmaNiBhriain/feeds/x-accelerometer", msg="90")
-    time.sleep(1)
-    print("sending value 20")
-    client.publish(topic="EmmaNiBhriain/feeds/x-accelerometer", msg="20")
-
-    time.sleep(1)
+    #print(li.roll())
+    roll = str(li.roll())
+    client.publish(topic="EmmaNiBhriain/feeds/x-accelerometer", msg=roll)
+    time.sleep(3)
+    roll = str(li.roll())
+    client.publish(topic="EmmaNiBhriain/feeds/x-accelerometer", msg=roll)
+    time.sleep(3)
